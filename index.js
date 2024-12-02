2515c0e68f698be27332d2cbf1748f16ea7969e6
dina-salah <dinasalah35586@gmail.com>
2024-12-01 20:47:48 -0700

    fist

---
+++ index.js
@@ -0,0 +1,14 @@
+var loginEmail = document.getElementById("loginEmail");
+var loginPass = document.getElementById("loginPass");
+var loginBtn = document.getElementById("loginBtn");
+// signup page
+var signUpName = document.getElementById("signUpName");
+var signUpEmail = document.getElementById("signUpEmail");
+var signUpPass = document.getElementById("signUpPass");
+var signUPBtn = document.getElementById("signUPBtn");
+// home page
+var username = document.getElementById("username");
+var Logout = document.getElementById("Logout");
+
+var users = [];
+//hidden sign up div within the same page using event "click" add hidden class to the login and remove the hidden from the sign up viseversa
