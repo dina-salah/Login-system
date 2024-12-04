94d7f63bbb3bd226b35071a85471a6120650123a
dina-salah <dinasalah35586@gmail.com>
2024-12-04 00:13:15 -0700

    log out and home page onprogress

--- index.js
+++ index.js
@@ -10,28 +10,42 @@ var signUpEmail = document.getElementById("signUpEmail");
 var signUpPass = document.getElementById("signUpPass");
 var signUPBtn = document.getElementById("signUPBtn");
 var go_to_login = document.getElementById("go_to_login");
+
+var staticBackdrop = new bootstrap.Modal(document.getElementById("staticBackdrop"));
 // home page
-var username = document.getElementById("username");
+var userName = document.getElementById("userName");
 var Logout = document.getElementById("Logout");
 
 var users = [];
 // fill the array from local storage
+if (localStorage.getItem(users) !== null) {
+    users = JSON.parse(localStorage.getItem(users))
+} else {
+    users = []
+}
 // regex pattern
 const userNameRegex = /^[a-zA-Z0-9_]{3,15}$/;
 const userEmailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const passwordRegex = /^(?=.[a-zA-Z]*)(?=.*\d)[a-zA-Z\d@!#$%&*]{8,16}$/ 
+const passwordRegex = /^(?=.[a-zA-Z]*)(?=.*\d)[a-zA-Z\d@!#$%&*]{8,16}$/;
+
 
 
 
 go_to_signup.addEventListener("click", function () {
     loginDiv.classList.add("hidden");
     signUpDiv.classList.remove("hidden");
+
+    signUpEmail.classList.remove("is-valid");
+    signUpName.classList.remove("is-valid");
+    signUpPass.classList.remove("is-valid");
+
+
 });
 go_to_login.addEventListener("click", function () {
     signUpDiv.classList.add("hidden");
     loginDiv.classList.remove("hidden");
 });
- 
+
 
 
 // create acc
@@ -42,32 +56,89 @@ signUPBtn.addEventListener("click", () => {
         signUpPass: signUpPass.value
     }
     // VALIDATION using regex
-    if ( userNameRegex.test(user_info.signUpName)   && userEmailRegex.test(user_info.signUpEmail) &&  passwordRegex.test(user_info.signUpPass)) {
+    if (userNameRegex.test(user_info.signUpName) && userEmailRegex.test(user_info.signUpEmail) && passwordRegex.test(user_info.signUpPass)) {
         users.push(user_info);
         go_to_login.click();
         reset();
         localStorage.setItem('users', JSON.stringify(users));
     } else {
-        // else if must be included using classadd add invaild to form control class
-        console.log('fill the form')
+        alert('erorr')
+    }
+})
+
+signUpName.addEventListener('input', function () {
+    isValid();
+});
+signUpEmail.addEventListener('input', function () {
+    emailvalidation();
+});
+signUpPass.addEventListener('input', function () {
+    passvalidation();
+});
+
+
+function isValid() {
+    // signUpName
+    if (userNameRegex.test(signUpName.value) == true) {
+
+        signUpName.classList.remove('is-invalid')
+        signUpName.classList.add('is-valid')
+
+    } else if (userNameRegex.test(signUpName.value) == false) {
+
+        signUpName.classList.add('is-invalid')
+        signUpName.classList.remove('is-valid')
 
     }
 
-})
+
+}
+
+function emailvalidation() {
+    if (userEmailRegex.test(signUpEmail.value) == true) {
+        console.log("tt")
+        signUpEmail.classList.remove('is-invalid')
+        signUpEmail.classList.add('is-valid')
+
+        console.log(`name true+ ${signUpEmail.value}`)
+    } else if (userEmailRegex.test(signUpEmail.value) == false) {
+        console.log("fffff")
+
+        signUpEmail.classList.add('is-invalid')
+        signUpEmail.classList.remove('is-valid')
+
+        console.log(`name false+ ${signUpEmail.value}`)
+    }
+}
+
+function passvalidation() {
 
 
+    if (passwordRegex.test(signUpPass.value) == true) {
+        signUpPass.classList.remove('is-invalid')
+        signUpPass.classList.add('is-valid')
+
+    } else if (passwordRegex.test(signUpPass.value) == false) {
+
+        signUpPass.classList.add('is-invalid')
+        signUpPass.classList.remove('is-valid')
+
+    }
+
+
+}
 // login
 loginBtn.addEventListener("click", () => {
     var users_list = JSON.parse(localStorage.getItem('users'));
     for (i = 0; i < users_list.length; i++) {
 
         if (loginEmail.value == users_list[i].signUpEmail && loginPass.value == users_list[i].signUpPass) {
-            // go to home page 
-            console.log('loged in successful')
+
+            window.location.href = '\home.html'
+            localStorage.setItem('current_user', users_list[i].signUpName)
             break;
         } else {
-            // show error message 
-            console.log('failed')
+            staticBackdrop.show();
         }
 
     }
@@ -82,4 +153,17 @@ function reset() {
     loginPass.value = '';
 }
 
-// log out from home page to index
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var name = localStorage.getItem('current_user')
+//     if(userName && name ){
+//         userName.innerHTML = name;
+//     console.log(name)
+//     }else{
+//     console.log('name error')
+        
+//     }
+
+// })
+// name in home
+//  logout
